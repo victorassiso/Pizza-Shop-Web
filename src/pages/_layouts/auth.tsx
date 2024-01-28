6c52965907519fae70520e43fe6b398283e95b27
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-28 20:51:42 -0300

    FEAT: Auth Layout Responsivity

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -3,19 +3,19 @@ import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
   return (
-    <div className="grid min-h-screen grid-cols-2 antialiased">
-      <div className="flex h-full flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
-        <div className="flex items-center gap-3 text-lg text-foreground">
+    <div className="flex h-screen flex-col items-center justify-center antialiased md:grid md:grid-cols-2">
+      <div className="flex justify-center md:h-full md:flex-col md:justify-between md:border-r md:border-foreground/5 md:bg-muted md:p-10 md:text-muted-foreground">
+        <div className="absolute left-8 top-8 flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
           <span className="font-semibold">pizza.shop</span>
         </div>
 
-        <footer className="text-sm">
+        <footer className="absolute bottom-2 w-full text-center text-sm">
           Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
 
-      <div className="relative flex flex-col items-center justify-center">
+      <div className="flex flex-col justify-between md:relative">
         <Outlet />
       </div>
     </div>
