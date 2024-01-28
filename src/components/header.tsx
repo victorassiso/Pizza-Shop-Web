80717d43190febefc1b9f2f3bd9921a0fafd85be
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-27 23:18:20 -0300

    FEAT: Header Responsivity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,32 +1,101 @@
-import { Barcode, Home, Pizza, Users, UtensilsCrossed } from 'lucide-react'
+import {
+  Barcode,
+  Home,
+  Menu,
+  Pizza,
+  Users,
+  UtensilsCrossed,
+} from 'lucide-react'
+import { useState } from 'react'
 
 import { AccountMenu } from './account-menu'
 import { NavLink } from './nav-link'
 import { ModeToggle } from './theme/mode-toggle'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu'
 import { Separator } from './ui/separator'
 
 export function Header() {
+  const [openDropdownMenu, setOpenDropdownMenu] = useState(false)
+
   return (
     <div className="border-r">
       <div className="flex h-16 items-center gap-6 px-6">
         <Pizza className="h-6 w-6" />
 
         <Separator orientation="vertical" />
-
-        <nav className="flex items-center space-x-4 lg:space-x-6">
-          <NavLink to="/">
+        {/* Drop Down Menu */}
+        <div className="lg:hidden">
+          <DropdownMenu open={openDropdownMenu}>
+            <DropdownMenuTrigger>
+              <Menu onClick={() => setOpenDropdownMenu(!openDropdownMenu)} />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="ml-24 md:hidden">
+              <NavLink
+                to="/"
+                className="rounded-sm p-2"
+                onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+              >
+                <Home className="h-4 w-4" />
+                Início
+              </NavLink>
+              <NavLink
+                to="/orders"
+                className="rounded-sm p-2"
+                onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+              >
+                <Barcode className="h-4 w-4" />
+                Pedidos
+              </NavLink>
+              <NavLink
+                to="/products"
+                className="rounded-sm p-2"
+                onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+              >
+                <UtensilsCrossed className="h-4 w-4" />
+                Produtos
+              </NavLink>
+              <NavLink
+                to="/customers"
+                className="rounded-sm p-2"
+                onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+              >
+                <Users className="h-4 w-4" />
+                Clientes
+              </NavLink>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        {/* Nav Bar */}
+        <nav className="hidden items-center space-x-4 md:flex lg:space-x-6">
+          <NavLink
+            to="/"
+            onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+          >
             <Home className="h-4 w-4" />
             Início
           </NavLink>
-          <NavLink to="/orders">
+          <NavLink
+            to="/orders"
+            onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+          >
             <Barcode className="h-4 w-4" />
             Pedidos
           </NavLink>
-          <NavLink to="/products">
+          <NavLink
+            to="/products"
+            onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+          >
             <UtensilsCrossed className="h-4 w-4" />
             Produtos
           </NavLink>
-          <NavLink to="/customers">
+          <NavLink
+            to="/customers"
+            onClick={() => setOpenDropdownMenu(!openDropdownMenu)}
+          >
             <Users className="h-4 w-4" />
             Clientes
           </NavLink>
--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,6 +1,8 @@
 import { Link, LinkProps, useLocation } from 'react-router-dom'
 
-export type NavLinkProps = LinkProps
+export interface NavLinkProps extends LinkProps {
+  className?: string
+}
 
 export function NavLink(props: NavLinkProps) {
   const { pathname } = useLocation()
@@ -8,8 +10,11 @@ export function NavLink(props: NavLinkProps) {
   return (
     <Link
       data-current={pathname === props.to}
-      className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground data-[current=true]:text-foreground"
       {...props}
+      className={
+        'flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground data-[current=true]:text-foreground ' +
+        props.className
+      }
     />
   )
 }
