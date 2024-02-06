891a246f0bf0a5c3936f523f06c19408473d59bb
Victor Assis <victor_assis@poli.ufrj.br>
2024-02-05 21:04:13 -0300

    FEAT: Product's Card List Skeleton

--- src/pages/app/products/components/mobile/product-card-list-skeleton.tsx
+++ src/pages/app/products/components/mobile/product-card-list-skeleton.tsx
@@ -1,7 +1,26 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
 export function ProductCardListSkeleton() {
-  return (
-    <div>
-      <h1>ProductCardListSkeleton</h1>
-    </div>
-  )
+  return Array.from({ length: 10 }).map((_, i) => {
+    return (
+      // <h1 key={i}>Test</h1>
+      <Card key={i}>
+        <CardHeader>
+          <div className="flex justify-between">
+            <div className="flex flex-col gap-1">
+              <Skeleton className="h-6 w-40" />
+              <Skeleton className="h-4 w-48" />
+            </div>
+            <Skeleton className="h-10 w-14" />
+          </div>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-1">
+          <Skeleton className="h-5 w-36" />
+          <Skeleton className="h-5 w-36" />
+          <Skeleton className="h-5 w-36" />
+        </CardContent>
+      </Card>
+    )
+  })
 }
