829ec7e9c18ed4337324b940ed89d7757f70d1d1
Victor Assis <victor_assis@poli.ufrj.br>
2024-02-22 10:49:57 -0300

    ADD: Loading message explaining delay on first request

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,9 +5,9 @@ export function NotFound() {
     <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="text-4xl font-bold">Página não encontrada</h1>
       <p className="text-accent-foreground">
-        Voltar para o{' '}
+        Voltar para a{' '}
         <Link to="/" className="text-sky-600 dark:text-sky-400">
-          Dashboard
+          Página Inicial
         </Link>
       </p>
     </div>
--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from 'react'
 import { MdDeliveryDining } from 'react-icons/md'
 import { ClipLoader } from 'react-spinners'
 
 export function Loading() {
+  const [showMessage, setShowMessage] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
   return (
-    <div className="relative flex h-screen w-full flex-col items-center justify-center text-foreground opacity-50">
-      <MdDeliveryDining size={80} />
-      <ClipLoader size={150} className="absolute" color="hsl(var(--text-" />
+    <div className="relative flex h-screen w-full flex-col items-center justify-center gap-20 text-foreground opacity-50">
+      <div className="relative flex items-center justify-center">
+        <MdDeliveryDining size={80} />
+        <ClipLoader size={150} className="absolute" color="hsl(var(--text-" />
+      </div>
+      {showMessage && (
+        <div className="mx-10 flex flex-col gap-3">
+          <span className="flex justify-center text-2xl font-bold">
+            ⚠ Atenção !
+          </span>
+          <p className="max-w-screen-md text-justify">
+            Este projeto é hospedado em um plano gratuito, o que pode causar um
+            atraso de até <strong>1 minuto</strong> no carregamento inicial. Mas
+            não se preocupe! Após a primeira requisição, as demais funcionarão
+            normalmente, sem atrasos. Pode confiar! 😉
+          </p>
+        </div>
+      )}
     </div>
   )
 }
